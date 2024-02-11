c4b8a20c71444040d7f12b41eff4ff1a2cf3949a
HeronErin <HeronErin@protonmail.com>
2024-02-11 18:24:16 -0500

    Add git test dir

---
+++ source/tests/filetests.d
@@ -0,0 +1,51 @@
+module filetests;
+import std.stdio;
+import falloc;
+import freck.streams.filestream;
+
+// Test basic falloc usage
+unittest{
+    auto allocator = new falloc.FileAlloc(new FileStream("basic.dust", "w+b"), true);
+
+    assert(25 == allocator.alloc(0));
+    assert(30 == allocator.alloc(0));
+    assert(35 == allocator.alloc(0));
+
+    allocator.free(25);
+    assert(25 == allocator.alloc(0));
+
+    assert(40 == allocator.alloc(100));
+    assert(145 == allocator.alloc(0));
+
+
+    try{ allocator.alloc(falloc.DEFAULT_SECTION_SIZE); }
+    catch (core.exception.AssertError e){
+        /* This is intended behavior! */
+        goto GOOD;
+    }
+    assert(0, "falloc did not throw an assertion error when allocing too much!");
+    GOOD:
+
+    assert(150 == allocator.alloc(5*1024*1024));
+    assert(10485797 == allocator.alloc(5*1024*1024));
+
+    "Passed basic usage test".writeln();
+    //allocator.printAllocTree();
+}
+
+
+// Opening a pre-written file test
+unittest{
+
+    auto allocator = new falloc.FileAlloc(new FileStream("reopen.dust", "wb+"), true);
+    assert(25 == allocator.alloc(100));
+    allocator.destroy();
+
+
+    allocator = new falloc.FileAlloc(new FileStream("reopen.dust", "rb+"), false);
+    assert(130 == allocator.alloc(100));
+
+    "Passed opening a pre-written file test".writeln();
+    //allocator.alloc(100).writeln();
+}
+
