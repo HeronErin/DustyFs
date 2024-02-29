bc2b7235166642d43aaafa1da842b0fe268fb58b
HeronErin <HeronErin@protonmail.com>
2024-02-29 14:03:52 -0500

    NO MORE BUFFERED STREAM

--- source/app.d
+++ source/app.d
@@ -13,21 +13,21 @@ import dustyfs.lazyload;
 import dustyfs.dirnode : DirNode;
 import dustyfs.filenode : FileNode;
 
-import bufferedStream;
+//import bufferedStream;
 
 void main(){
-    auto stream = new MemoryStream(new ubyte[0]);
-    auto s2 = new BufferedStream(stream);
-    s2.write([1]);
-
-    s2.seek(10);
-    s2.write([9]);
-
-    s2.seek(0);
-    s2.bufferChuncks.writeln;
-    s2.write(cast(ubyte[]) "Some test text that is bound to be quite long for this test");
-    s2.flush();
-    stream.seek(0);
-    stream.getContents.writeln;
+    //auto stream = new MemoryStream(new ubyte[0]);
+    //auto s2 = new BufferedStream(stream);
+    //s2.write([1]);
+    //
+    //s2.seek(10);
+    //s2.write([9]);
+    //
+    //s2.seek(0);
+    //s2.bufferChuncks.writeln;
+    //s2.write(cast(ubyte[]) "Some test text that is bound to be quite long for this test");
+    //s2.flush();
+    //stream.seek(0);
+    //stream.getContents.writeln;
 
 }
